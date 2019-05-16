dd3160aab89945ddce2045bf7547a2a7e19ad83b
jhorology <jhorology2014@gmail.com>
2019-05-16 18:48:06 +0900

    just a update

--- clip-mapper-example.js
+++ clip-mapper-example.js
@@ -9,14 +9,14 @@ const log = require('fancy-log');
  */
 module.exports = function(soundInfo) {
   // check soundInfo
-  log.info('Sound Info', soundInfo);
+  log.info('custom mapper', 'Sound Info:', soundInfo);
   let clip;
   if (soundInfo.types[0][0] === 'Bass') {
     clip = path.resolve(__dirname, 'Bitwig Studio Files/NKS-Preview-C1-Single.bwclip'); 
   } else {
     clip = path.resolve(__dirname, 'Bitwig Studio Files/NKS-Preview-Cmaj-Chord.bwclip');
   }
-  log.info('MIDI Clip', clip);
+  log.info('custom mapper', 'MIDI clip:', clip);
   // shuld return a absolute path, Bitwig Studio can not knows about relative path.
   return clip;
 };
