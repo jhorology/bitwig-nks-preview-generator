f0dd12080d5430b970c2d469c2724082119b7b34
jhorology <jhorology2014@gmail.com>
2019-05-17 17:10:44 +0900

    just a update

--- clip-mapper-example.js
+++ clip-mapper-example.js
@@ -5,18 +5,19 @@ const log = require('fancy-log');
  * Example NKS Preview MIDI clip mapper for UVI Key Suite Digital.
  * 
  * @param {Object} soundInfo - NKS Sound Info (metadata).
- * @return {String} - absolute path of Bitwig Studio MIDI clip.
+ * @return {String} - Bitwig Studio MIDI clip file path.
  */
 module.exports = function(soundInfo) {
   // check soundInfo
   log.info('custom mapper', 'Sound Info:', soundInfo);
   let clip;
   if (soundInfo.types[0][0] === 'Bass') {
+    // return absolute path
     clip = path.resolve(__dirname, 'Bitwig Studio Files/NKS-Preview-C1-Single.bwclip'); 
   } else {
-    clip = path.resolve(__dirname, 'Bitwig Studio Files/NKS-Preview-Cmaj-Chord.bwclip');
+    // or relative path from this .js file's directory.
+    clip = 'Bitwig Studio Files/NKS-Preview-Cmaj-Chord.bwclip';
   }
   log.info('custom mapper', 'MIDI clip:', clip);
-  // shuld return a absolute path, Bitwig Studio can not knows about relative path.
   return clip;
 };
